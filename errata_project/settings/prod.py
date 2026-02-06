20ef88dfc644f945f0afffa8d2412b3782038402
Jennifer Richards <jennifer@staff.ietf.org>
2026-02-06 18:44:14 +0000

    chore: set ALLOWED_HOSTS

--- errata_project/settings/prod.py
+++ errata_project/settings/prod.py
@@ -18,6 +18,9 @@ assert not SECRET_KEY.startswith(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# ERRATA_ALLOWED_HOSTS is a newline-separated list of allowed hosts
+ALLOWED_HOSTS = _multiline_to_list(os.environ["ERRATA_ALLOWED_HOSTS"])
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
