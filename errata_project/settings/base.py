58f03d9ad4447e72a0cc9d78e2bdc0dd0bfd2be6
Jennifer Richards <jennifer@staff.ietf.org>
2026-02-06 17:02:40 +0000

    chore: remove unused/shadowed DATABASES value

--- errata_project/settings/base.py
+++ errata_project/settings/base.py
@@ -91,17 +91,6 @@ SESSION_COOKIE_NAME = (
 LOGIN_REDIRECT_URL = "/user-info/"
 LOGOUT_REDIRECT_URL = "/"
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
